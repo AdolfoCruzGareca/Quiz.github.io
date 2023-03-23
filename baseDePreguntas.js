d0821b0b9504832500bbb1ccfe080410c4d80ff8
AdolfoCruzGareca <127909668+AdolfoCruzGareca@users.noreply.github.com>
2023-03-23 20:55:51 -0300

    Initial Commit

---
+++ baseDePreguntas.js
@@ -0,0 +1,33 @@
+baseDePreguntas = [
+  {
+    pregunta: `¿Cuál fue la empresa que invento el celular?`,
+    imagen: `https://i.ibb.co/TrJ1bss/telefono-celular.jpg`,
+    respuesta: `Motorola`,
+    distractores: [`Samsung`, `Nokia`,`Sony-Ericsson`],
+  },               
+
+{
+    pregunta: `¿Quién invento el Cálculo Diferencial?`,
+    imagen:`https://i.ibb.co/cL0nnQm/calculo-diferencial.webp`,
+    respuesta: `Isacc Newton`,
+    distractores: [`Blas Pascal`, `Gottfried Leibniz`, `Albert Einstein`],
+    ayuda:`También invento las Leyes de la Dinámica`,      
+    ayudaImg: `https://i.ibb.co/JFY1PqF/leyes-de-dinamica.jpg`,
+  },
+  {
+    pregunta: `¿Quén invento el Cálculo Integral?`,
+    imagen:`https://i.ibb.co/xmYBfFS/calculo-integral.jpg`,
+    respuesta: `Jacob Bernoulli`,
+    distractores: [`Blas Pascal`, `Albert Einstein`,`Isaac Newton`],
+    ayuda: `También estudio el flujo de fluidos`,
+    ayudaImg: `https://i.ibb.co/8zzFfV6/flujo-de-fluidos.jpg`,
+  },
+  {
+    pregunta: `¿Quén descubrio el Atomo?`,
+    imagen:`https://i.ibb.co/7VCVVQ8/atomo.jpg`,
+    respuesta: `John Dalton`,
+    distractores: [`Leucippus`, `Democrito`,`Louis Pasteur`],
+    ayuda: `También invento las Leyes de los Gases`,
+    ayudaImg: `https://i.ibb.co/JtCkLjh/leyes-de-los-gases.jpg`,
+  }     
+];
